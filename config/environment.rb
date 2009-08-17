2647e04619598606479eeaa2f84967d069fa6cc2
tekkyjim <tekkyjim@gmail.com>
2009-08-17 21:20:46 +0100

    finally, back on track and upgraded to rails 2.3.3

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "rspec", :lib => false, :version => ">= 1.2.4"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.4"
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => ">= 1.2.1"
-  
+  config.gem 'test-unit', :lib => 'test/unit'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
